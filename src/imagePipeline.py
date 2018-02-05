1a3fe60457dd2f87ef7a4435ff82cc7b16843eee
donagha <donagha@wwu.edu>
2018-02-05 10:54:56 -0800

    "Expected int32 got _message" error, idk how to fix

--- src/imagePipeline.py
+++ src/imagePipeline.py
@@ -3,15 +3,20 @@ import tensorflow as tf
 import os
 import sys
 
+# trainCats = tf.constant([("../resources/training_set/cats/cat.%d.jpg" % i) for i in range(1,4000)], dtype=tf.string)
+# trainDogs = tf.constant([("../resources/training_set/dogs/dog.%d.jpg" % i) for i in range(1,4000)], dtype=tf.string)
+# trainingSet = tf.concat([trainCats, trainDogs], 0)
+
 
 def readJPEG(filenameQue):
-        randX = random.randint(0, 100)
-        randY = random.randint(0, 100)
-
         label = tf.Variable([0], dtype=tf.int8)
         reader = tf.WholeFileReader()
         key, recordString = reader.read(filenameQue)
         examplePreProcess = tf.image.decode_jpeg(recordString)
+        exampleShape = tf.shape(example)
+        randX = random.randint(0, (exampleShape[0] - 32))
+        randY = random.randint(0, (exampleShape[1] - 32))
+
         example = tf.reshape(tf.strided_slice(examplePreProcess, [randX,randY,0], [randX + 32, randY + 32, 3]), [32, 32, 3])
         return example, key
 
@@ -21,11 +26,9 @@ def inputPipeline(filenames, batchSize):
         filenameQueue = tf.train.string_input_producer(filenames)
         example, label = readJPEG(filenameQueue)
         capacity = minAfterDequeue + (numThreads + 3) * batchSize
-        exampleBatch, labelBatch = tf.train.shuffle_batch([example, label], 
+        exampleBatch, labelBatch = tf.train.shuffle_batch([example, label],
                                                         batch_size=batchSize,
                                                         num_threads=numThreads,
-                                                        capacity=capacity, 
+                                                        capacity=capacity,
                                                         min_after_dequeue=minAfterDequeue)
         return exampleBatch, labelBatch
-
-
--- src/main.py
+++ src/main.py
@@ -2,8 +2,8 @@ import tensorflow as tf
 import imagePipeline
 
 #get the training images paths
-trainCats = tf.constant([("../resources/training_set/cats/cat.%d.jpg" % i) for i in range(1,4000)])
-trainDogs = tf.constant([("../resources/training_set/dogs/dog.%d.jpg" % i) for i in range(1,4000)])
+trainCats = tf.constant([("../resources/training_set/cats/cat.%d.jpg" % i) for i in range(1,4000)], dtype=tf.string)
+trainDogs = tf.constant([("../resources/training_set/dogs/dog.%d.jpg" % i) for i in range(1,4000)], dtype=tf.string)
 trainingSet = tf.concat([trainCats, trainDogs], 0)
 
 #create batches
