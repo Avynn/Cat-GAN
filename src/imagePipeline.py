def53a3a54fcea086bdf99555599e4bb4dac86c2
donagha <donagha@wwu.edu>
2018-02-06 13:50:56 -0800

    have absolutely no idea what is going on with my python, have to leave lab though

--- src/imagePipeline.py
+++ src/imagePipeline.py
@@ -6,28 +6,38 @@ import sys
 SHAPE = 64 * 64 * 3
 
 
-def readExamples(filenameQue):
-        reader = tf.TFRecordReader()
-        _, serializedOutput = reader.read(filenameQue)
-        print(type(serializedOutput))
-        example = tf.train.Example()
-        example.ParseFromString(serializedOutput)
-        label = int(example.features.feature['label'].int64_list.value[0])
-        imgBytes = example.features.feature['rawImage'].bytes_list.value[0]
-        image = tf.decode_raw(imgBytes)
-        image.set_shape(SHAPE)
+# def readExamples(filenameQue):
+#         reader = tf.TFRecordReader()
+#         _, serializedOutput = reader.read(filenameQue)
+#         print(type(serializedOutput))
+#         example = tf.train.Example()
+#         example.ParseFromString(serializedOutput)
+#         label = int(example.features.feature['label'].int64_list.value[0])
+#         imgBytes = example.features.feature['rawImage'].bytes_list.value[0]
+#         image = tf.decode_raw(imgBytes)
+#         image.set_shape(SHAPE)
+#
+#         return image, label
+#
+# def inputPipeline(filename, batchSize, numEpochs):
+#         minAfterDequeue = 1000
+#         numThreads = 3
+#         filenameQueue = tf.train.string_input_producer([filename], num_epochs=numEpochs)
+#         example, label = readExamples(filenameQueue)
+#         capacity = minAfterDequeue + numThreads + 3 * batchSize
+#         exampleBatch, labelBatch = tf.train.shuffle_batch([example, label],
+#                                                         batch_size=batchSize,
+#                                                         num_threads=numThreads,
+#                                                         capacity=capacity,
+#                                                         min_after_dequeue=minAfterDequeue)
+#         return exampleBatch, labelBatch
 
-        return image, label
 
-def inputPipeline(filename, batchSize, numEpochs):
-        minAfterDequeue = 1000
-        numThreads = 3
-        filenameQueue = tf.train.string_input_producer([filename], num_epochs=numEpochs)
-        example, label = readExamples(filenameQueue)
-        capacity = minAfterDequeue + numThreads + 3 * batchSize
-        exampleBatch, labelBatch = tf.train.shuffle_batch([example, label],
-                                                        batch_size=batchSize,
-                                                        num_threads=numThreads,
-                                                        capacity=capacity,
-                                                        min_after_dequeue=minAfterDequeue)
-        return exampleBatch, labelBatch
+def parseRecords(exapmlePrototype):
+    features = {
+        'label': tf.FixedLenFeature((), tf.int64, default_value=0),
+        'rawImage': tf.FixedLenFeature((), tf.string, default_value="")
+    }
+    parsedFeatures = tf.parse_single_example(exapmlePrototype, features)
+    image = tf.decode_raw(parsedFeatures['rawImage'], tf.uint8)
+    return image, parsedFeatures['label']
--- src/main.py
+++ src/main.py
@@ -1,28 +1,34 @@
 import tensorflow as tf
 import imagePipeline
 
-
 #get the training images paths
 # trainCats = tf.constant([("../resources/training_set/cats/cat.%d.jpg" % i) for i in range(1,4000)], dtype=tf.string)
 # trainDogs = tf.constant([("../resources/training_set/dogs/dog.%d.jpg" % i) for i in range(1,4000)], dtype=tf.string)
 # trainingSet = tf.concat([trainCats, trainDogs], 0)
 
 #create batches
-inputImages, inputLabels = imagePipeline.inputPipeline("../resources/training.tfrecords", 100, 1)
+# inputImages, inputLabels = imagePipeline.inputPipeline("../resources/training.tfrecords", 100, 1)
+
+dataset = tf.contrib.data.TFRecordDataset('../resources/training.tfrecords')
+datatset = dataset.map(imagePipeline.parseRecords)
+dataset = dataset.batch(32)
+iterator = dataset.make_initializable_iterator()
+nextBatch = iterator.get_next()
 
 with tf.Session() as sess:
         #init variables in session
         tf.global_variables_initializer().run()
         tf.local_variables_initializer().run()
 
+        sess.run(iterator.initializer)
         #init coordinator and start queing dem bois up!
-        coord = tf.train.Coordinator()
-        threads = tf.train.start_queue_runners(coord=coord)
-        sess.run([inputImages])
-        sess.run([inputLabels])
-
-        print(inputImages)
+        # coord = tf.train.Coordinator()
+        # threads = tf.train.start_queue_runners(coord=coord)
+        printList = []
+        sess.run([nextBatch])
+        
+        # print(inputImages)
 
         #deinit coord
-        coord.request_stop()
-        coord.join(threads)
+        # coord.request_stop()
+        # coord.join(threads)
