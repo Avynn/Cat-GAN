a8abc88fc4d3eda105beb52c6bbbad529e820a96
Avynn Donaghe <avynn@donaghe.com>
2018-02-05 17:59:40 -0800

    Successfully got an image's data into a numpy array
    the next steps are to convert that to a tensor and use tensorflow's file type to convert these images to something useful to tensorflow.

--- src/ImageConverter.py
+++ src/ImageConverter.py
@@ -1,6 +1,8 @@
 from PIL import Image
+import numpy as np
 
-imageData = []
+
+imageData = np.zeros((64,64,3))
 
 dogImage = Image.open("../resources/training_set/dogs/dog.1.jpg")
 dogImage = dogImage.resize((64,64))
@@ -9,13 +11,14 @@ dogPixels = dogImage.load()
 imgX = 0
 imgY = 0
 
-for i in range(0,((64 * 64) - 1)):
-    print((imgX, imgY))
+for i in range(0,(64 * 64)):
     if(imgX > 63):
         imgX = 0
         imgY += 1
-    else:
-        imageData.append(dogPixels[imgX, imgY])
-        imgX += 1
+    for j in range(0,3):
+        imageData[imgX][imgY][j] = dogPixels[imgX, imgY][j]
+    imgX += 1
+        
 
+print(imageData)
 print("done copying!")
