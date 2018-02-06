0a7a573740ef113b7239c4adf2768c7fa50cd86e
Avynn Donaghe <avynn@donaghe.com>
2018-02-05 21:13:12 -0800

    Getting closer to those tfRecords
    getting closer... it looks like actual code now lol.

--- src/ImageConverter.py
+++ src/ImageConverter.py
@@ -1,42 +1,57 @@
 from PIL import Image
 import numpy as np
+import tensorflow as tf
 import argparse
 import os
 import glob
 import sys
 import io
 
-FLAGS = None
+SHAPEX = 64
+SHAPEY = 64
 
-imageData = np.zeros((64,64,3))
+def int64Feature(val):
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[val]))
 
-dogImage = Image.open("../resources/training_set/dogs/dog.1.jpg")
-dogImage = dogImage.resize((64,64))
-dogPixels = dogImage.load()
-dogPixelsToBytes = io.BytesIO()
-dogImage.save(dogPixelsToBytes, format="png")
+def bytesFeature(val):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[val]))
 
 def convertImages(folderPath, name):
-    knowDirectories = []
+    knownDirectories = []
+    outFile = "../resources/" + name + ".tfrecords"
+    print("writing %s" % name)
+    writer = tf.python_io.TFRecordWriter(outFile)
     i = 0
     for (dirPath, dirNames, fileNames) in os.walk(folderPath):
         if (i == 0):
-            knowDirectories = dirNames 
+            for j in range(len(dirNames)):
+                knownDirectories.append(dirNames[j])
+        else:
+            print(knownDirectories)
+            print(i)
+            for fileName in fileNames:
+                label = 0
+                path = folderPath + '/' + knownDirectories[i - 1] + "/" + fileName
+                image = Image.open(path)
+                image = image.resize((SHAPEX,SHAPEY))
+                imageToBytes = io.BytesIO()
+                image.save(imageToBytes, format="png")
+                if("cat" in fileName):
+                    label += 1
+                example = tf.train.Example(features = tf.train.Features(feature = {
+                    'height': int64Feature(SHAPEY),
+                    'width': int64Feature(SHAPEX),
+                    'depth': int64Feature(3),
+                    'label': int64Feature(label),
+                    'rawImage': bytesFeature(imageToBytes)
+                }))
+                writer.write(example.serializeToString())
+
         i += 1
-    print(knowDirectories)
-    print(i)
+    writer.close()
 
-# imgX = 0
-# imgY = 0
+def main(argv):
+    convertImages('../resources/training_set', "training")
+    convertImages('../resources/test_set', 'testing')
 
-# for i in range(0,(64 * 64)):
-#     if(imgX > 63):
-#         imgX = 0
-#         imgY += 1
-#     for j in range(0,3):
-#         imageData[imgX][imgY][j] = dogPixels[imgX, imgY][j]
-#     imgX += 1
-
-convertImages("../resources/training_set/", 1)
-
-print("ran!")
+tf.app.run(main=main, argv=[sys.argv[0]])
